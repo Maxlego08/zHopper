638c5e3eaa86f2d9b4d5d0984aaf4b0cb94191b7
Maxlego08 <maxlego08@sfr.fr>
2020-04-16 18:21:13 +0200

    Ajout de wildstacker

--- src/fr/maxlego08/hopper/modules/ModuleKillMob.java
+++ src/fr/maxlego08/hopper/modules/ModuleKillMob.java
@@ -3,6 +3,7 @@ package fr.maxlego08.hopper.modules;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Animals;
@@ -10,10 +11,15 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 
+import com.bgsoftware.wildstacker.api.WildStackerAPI;
+import com.bgsoftware.wildstacker.api.objects.StackedEntity;
+
 import fr.maxlego08.hopper.api.Hopper;
 import fr.maxlego08.hopper.api.Level;
 
 public class ModuleKillMob extends Module {
+
+	private final boolean isWildStacker = Bukkit.getPluginManager().isPluginEnabled("WildStacker");
 
 	public ModuleKillMob(int priority) {
 		super(priority);
@@ -49,14 +55,52 @@ public class ModuleKillMob extends Module {
 
 		int amount = 0;
 		for (LivingEntity entity : stream.collect(Collectors.toList())) {
-			
+
 			if (amount > maxPerSecond)
 				return;
-			
-			entity.teleport(location);
-			entity.damage(entity.getHealth() * 10);
-			amount++;
+
+			if (isWildStacker) {
+
+				StackedEntity stackedEntity = WildStackerAPI.getStackedEntity(entity);
+				if (stackedEntity == null) {
+					entity.teleport(location);
+					entity.damage(entity.getHealth() * 10);
+					amount++;
+					continue;
+				}
+
+				int stackedAmount = stackedEntity.getStackAmount();
+
+				// Si le nombre d'entity est un alors on s'en fou
+				if (stackedAmount == 1) {
+
+					entity.teleport(location);
+					entity.damage(entity.getHealth() * 10);
+					amount++;
+
+				} else
+
+					for (int a = 0; a < stackedAmount; a++) {
+
+						if (amount > maxPerSecond)
+							return;
+
+						stackedAmount = stackedAmount - 1;
+						stackedEntity.setStackAmount(stackedAmount, true);
+						LivingEntity spawnedEntity = (LivingEntity) entity.getWorld().spawnEntity(entity.getLocation(),
+								stackedEntity.getType());
+						spawnedEntity.teleport(location);
+						spawnedEntity.damage(entity.getHealth() * 10);
+						amount++;
+
+					}
+			} else {
+				entity.teleport(location);
+				entity.damage(entity.getHealth() * 10);
+				amount++;
+			}
 		}
 
 	}
+
 }
